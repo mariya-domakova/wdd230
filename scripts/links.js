b2276455899c8cba0188224a57e27b1ebcd87f36
mariya-domakova <mashadom46@gmail.com>
2023-11-10 03:10:05 +0300

    homepage update

---
+++ scripts/links.js
@@ -0,0 +1,32 @@
+const baseURL = "https://mariya-domakova.github.io/wdd230/";
+const linksURL = "https://mariya-domakova.github.io/wdd230/data/links.json";
+const linksList = document.querySelector("#links");
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach(week => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${week.lesson}: `;
+        const innerLinks = week.links;
+        innerLinks.forEach((link) => {
+            const listTag = document.createElement("a");
+            listTag.setAttribute("href", link.url);
+            listTag.setAttribute("target", "_blank");
+            listTag.innerText = `${link.title}`;
+            listItem.appendChild(listTag);
+            if (link != innerLinks.at(-1)) {
+                const separator = document.createTextNode(" | ");
+                separator.style.fontWeight = "thin";
+                listItem.appendChild(separator);
+            }
+        })
+        linksList.appendChild(listItem);
+    });
+}
+
+getLinks();
