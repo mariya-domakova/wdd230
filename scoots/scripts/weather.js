17851285023bdb8978b01afb04bba6062f988382
mariya-domakova <mashadom46@gmail.com>
2023-12-20 22:03:02 +0300

    update

--- scoots/scripts/weather.js
+++ scoots/scripts/weather.js
@@ -1,33 +1,3 @@
 // const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.50&lon=-86.94&units=metric&appid=c510d1005d644350d657a791ad894ed7';
 
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-
-const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=20.50&lon=-86.94&units=metric&appid=c510d1005d644350d657a791ad894ed7';
-
-async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json(); 
-            console.log(data);
-            // displayResults(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
-  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  let desc = data.weather[0].description;
-  weatherIcon.setAttribute('src', iconsrc);
-  weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = `${desc}`;
-}
-
 apiFetch();
